89803005c02858a782be6aaf9254c687cddd6120
Karl Palsson <karlp@tweak.net.au>
2021-04-23 12:52:28 +0000

    make_web_videos: overhaul and add webm
    Make it a little nicer script, add support for two pass webm+vp9.
    
    nice videos, small, slow to make...

--- make_web_videos.sh
+++ make_web_videos.sh
@@ -1,61 +1,79 @@
 #!/bin/sh
 # convert any movie down into two constrained size web compatible files, on mp4 and on ogv.
 # now you need the details from: http://camendesign.com/code/video_for_everybody
+DO_OGV=no
+DO_MP4=no
+DO_WEBM=no
 
-INFILE=$1
-OUTFILE_MP4=$2
-OUTFILE_OGV=$3
+usage() {
+    printf "%s -i input_file -o output_file_basename [options]\n" "$0"
+    printf "\t-g\tEnable OGV creation (output to basename.ogv)\n"
+    printf "\t-m\tEnable MP4 creation (output to basename.mp4)\n"
+    printf "\t-w\tEnable WEBM creation (output to basename.webm)\n"
+}
+
+while getopts "i:o:ghmw" c
+do
+    case $c in
+    i) INFILE=$OPTARG ;;
+    o) OUTFILE=$OPTARG;;
+    g) DO_OGV=yes;;
+    m) DO_MP4=yes;;
+    w) DO_WEBM=yes;;
+    h|*) usage;;
+    esac
+done
+
 
 if [ -z "$INFILE" ]
 then
-    echo "Usage is <program> <infile> [outfile]"
-    echo "outfile will default if not given, and will never overwrite"
+    echo "No input file given!"
     exit 1
 fi
 
-if [ ! -e "$INFILE" ]
+if [ -z "$OUTFILE" ]
 then
-    echo "$INFILE doesn't exist!" 
+    echo "No output file given!"
     exit 1
 fi
 
-if [ -z "$OUTFILE_MP4" ] 
-then
-    OUTFILE_MP4=${INFILE}.mp4
-    echo "Defaulting mp4 output to: $OUTFILE_MP4";
-fi
 
-if [ -z "$OUTFILE_OGV" ]
-then
-    OUTFILE_OGV=${INFILE}.ogv
-    echo "Defaulting ogv output to: $OUTFILE_OGV";
-fi
+mk_ogv() {
+    ifn="$1"
+    ofn="$2".ogv
+    [ -e "$ofn" ] && {
+        printf "Output file %s already exists, skipping.\n" "$ofn"
+        return 1
+    }
+    ffmpeg -i "$ifn" -codec:v libtheora -qscale:v 7 -codec:a libvorbis -qscale:a 2 -filter_complex "scale=iw*min(1\,min(1280/iw\,720/ih)):-1" "$ofn"
+}
 
-if [ ! -e "$OUTFILE_OGV" ]
-then
-    echo "Generating web ogv file: $OUTFILE_OGV";
-#    ffmpeg2theora --two-pass --optimize --videoquality 6 --audioquality 1 --max_size 640x640 "$INFILE" -o "$OUTFILE_OGV"
-    # from: http://superuser.com/a/794924/138276
-    ffmpeg -i "$INFILE" -codec:v libtheora -qscale:v 7 -codec:a libvorbis -qscale:a 2 \
-	-filter_complex "scale=iw*min(1\,min(1280/iw\,720/ih)):-1" "$OUTFILE_OGV"
-else
-    echo "$OUTFILE_OGV already exists, refusing to trample!"
-fi
-if [ ! -e "$OUTFILE_MP4" ]
-then
-    echo "Generating web mp4 file: $OUTFILE_MP4";
-    #HandBrakeCLI --preset "Classic"  --width 640 -X 640 -vb 600 --input "$INFILE" --output "$OUTFILE_MP4"
-    # fuck knows what the difference is.
-    #HandBrakeCLI --preset "Normal"  --width 640 -X 640 -vb 600 --two-pass --turbo --input "$INFILE" --output "$OUTFILE_MP4"
-    # this forces it to only 480 wide, but qt is too high on cpu anyway, so that's ok. (width settings are ignored with this preset)
-    #HandBrakeCLI --no-dvdnav --preset "iPhone & iPod Touch"  -O -vb 600 --two-pass --turbo --input "$INFILE" --output "$OUTFILE_MP4"
-    HandBrakeCLI --no-dvdnav --preset "HQ 720p30 Surround"  -O --two-pass --turbo --input "$INFILE" --output "$OUTFILE_MP4"
-else
-    echo "$OUTFILE_MP4 already exists, refusing to trample!"
-fi
+mk_webm() {
+    ifn="$1"
+    ofn="$2".webm
+    [ -e "$ofn" ] && {
+        printf "Output file %s already exists, skipping.\n" "$ofn"
+        return 1
+    }
+    #ffmpeg -i "$ifn" -vcodec libvpx -acodec libvorbis -filter_complex "scale=iw*min(1\,min(1280/iw\,720/ih)):-1" "$ofn"
+    #ffmpeg -i "$ifn" -vcodec libvpx-vp9 -acodec libvorbis -filter_complex "scale=iw*min(1\,min(1280/iw\,720/ih)):-1" "$ofn"
 
-# maximum compatibility
-# TODO: these video sizes might be a bit big?
-# iphone forces to max 480 wide  :(
-#HandBrakeCLI --preset "iPhone & iPod Touch"  -O --width 640 -vb 600 --two-pass --turbo --input "$INFILE" --output "$OUTFILE_MP4"
+    ffmpeg -i "$ifn" -c:v libvpx-vp9 -b:v 2M -pass 1 -an -f null /dev/null && \
+    ffmpeg -i "$ifn" -c:v libvpx-vp9 -b:v 2M -pass 2 -c:a libopus "$ofn"
+}
 
+mk_mp4() {
+    ifn="$1"
+    ofn="$2".mp4
+    [ -e "$ofn" ] && {
+        printf "Output file %s already exists, skipping.\n" "$ofn"
+        return 1
+    }
+    HandBrakeCLI --no-dvdnav --preset "HQ 720p30 Surround"  -O --two-pass --turbo --input "$ifn" --output "$ofn"
+}
+
+[ $DO_OGV = "yes" ] && mk_ogv $INFILE $OUTFILE
+[ $DO_MP4 = "yes" ] && mk_mp4 $INFILE $OUTFILE
+[ $DO_WEBM = "yes" ] && mk_webm $INFILE $OUTFILE
+
+exit 0;
